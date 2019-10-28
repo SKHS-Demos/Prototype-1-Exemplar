0acb546b5b4db1fa42bcf21885a0552b4b786ef0
Douglas Urner <dlu@canishe.com>
2019-10-27 22:59:42 -0700

    Add player controls.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 20.0f; // Full throttle.
+    [SerializeField] private float yawRate = 10.0f; // Degrees per second.
 
     // Start is called before the first frame update
     void Start()
@@ -13,10 +14,17 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    [SerializeField] private float vertInput;
+    [SerializeField] private float horzInput;
     void Update()
     {
+        vertInput = Input.GetAxis("Vertical");
+        horzInput = Input.GetAxis("Horizontal");
+
         // Move the vehicle forward. Time.deltaTime normalizes
         // speed to meters per second.
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(speed * vertInput * Time.deltaTime * Vector3.forward);
+        transform.Rotate(yawRate * horzInput * Time.deltaTime * Vector3.up);
     }
 }
