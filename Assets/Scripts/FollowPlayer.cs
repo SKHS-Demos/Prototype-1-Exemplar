d25247347e332fd94fb3db9a1a393c9bdc820f14
Douglas Urner <dlu@canishe.com>
2019-10-27 22:28:44 -0700

    Add FollowPlayer script.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject vehicle;
+    [SerializeField] private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Calculate the distance between the camara and the vehicle.
+        offset = vehicle.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = vehicle.transform.position - offset;
+    }
+}
