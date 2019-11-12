c2e5af65be3e22d61004f35115d111bbb9721cf7
Douglas Urner <dlu@canishe.com>
2019-11-12 09:04:05 -0800

    Start scope examples.

---
+++ Assets/Scripts/Scope.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scope : MonoBehaviour
+{
+    public float outer = 42.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        public float inner = 24.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float local = outer + inner;
+    }
+}
